003c18289f5e55cdeb18bd53b76193239b63f141
Shreya Porwal <111834212+porwalshreyaa@users.noreply.github.com>
2024-07-05 02:30:50 +0530

    Update binary_tree.py

--- binary_tree.py
+++ binary_tree.py
@@ -17,3 +17,23 @@ link to my notes on doc:
 
 
 #bfs traversal 
+
+#level order traversal - queue
+class Node:
+    def __init__(self, key):
+        self.data = key
+        self.left = None
+        self.right = None
+
+def LevelOrder(root):
+    if root is None:
+        return
+    queue = []
+    queue.append(root)
+
+    while(len(queue)>0):
+        node = queue.pop(0)
+        if node.left is not None:
+            queue.append(node.left)
+        if node.right is not None:
+            queue.append(node.right)
