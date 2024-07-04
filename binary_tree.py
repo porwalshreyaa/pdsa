ac4ddf89b381e4805bf372e539cbac529b9c40eb
Shreya Porwal <111834212+porwalshreyaa@users.noreply.github.com>
2024-07-05 02:51:00 +0530

    Update binary_tree.py

--- binary_tree.py
+++ binary_tree.py
@@ -20,20 +20,46 @@ link to my notes on doc:
 
 #level order traversal - queue
 class Node:
-    def __init__(self, key):
+    def __init__(self,key=0,left=0,right=0):
         self.data = key
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
 
-def LevelOrder(root):
+class Queue:
+    def __init__(self):
+        self.items = []
+    def is_empty(self):
+        return len(self.items) ==0
+    def enqueue(self, item):
+        self.items.append(item)
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        return None
+
+
+def LevelOrderTraversal(root):
     if root is None:
-        return
-    queue = []
-    queue.append(root)
+        return []
+    result= []
+    queue = Queue()
+    queue.enqueue(root)
+    queue.enqueue(None)
 
-    while(len(queue)>0):
-        node = queue.pop(0)
-        if node.left is not None:
-            queue.append(node.left)
-        if node.right is not None:
-            queue.append(node.right)
+    current_level = []
+
+    while not queue.is_empty():
+
+        node = queue.dequeue()
+        if node:
+            current_level.append(node.value)
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+        else:
+            result.append(current_level)
+            current_level = []
+            if not queue.is_empty():
+                queue.enqueue(None)
+    return result 
