1249bd09869f20833b921b7d80bd7dc65d569121
Shreya Porwal <111834212+porwalshreyaa@users.noreply.github.com>
2024-07-09 02:40:01 +0530

    Update binary_tree.py

--- binary_tree.py
+++ binary_tree.py
@@ -19,47 +19,31 @@ link to my notes on doc:
 #bfs traversal 
 
 #level order traversal - queue
-class Node:
-    def __init__(self,key=0,left=0,right=0):
-        self.data = key
+
+from collections import deque
+from typing import List, Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
         self.left = left
         self.right = right
 
-class Queue:
-    def __init__(self):
-        self.items = []
-    def is_empty(self):
-        return len(self.items) ==0
-    def enqueue(self, item):
-        self.items.append(item)
-    def dequeue(self):
-        if not self.is_empty():
-            return self.items.pop(0)
-        return None
-
-
-def LevelOrderTraversal(root):
-    if root is None:
-        return []
-    result= []
-    queue = Queue()
-    queue.enqueue(root)
-    queue.enqueue(None)
-
-    current_level = []
-
-    while not queue.is_empty():
-
-        node = queue.dequeue()
-        if node:
-            current_level.append(node.value)
-            if node.left:
-                queue.enqueue(node.left)
-            if node.right:
-                queue.enqueue(node.right)
-        else:
-            result.append(current_level)
-            current_level = []
-            if not queue.is_empty():
-                queue.enqueue(None)
-    return result 
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        ans = []
+        if root is None:
+            return ans
+        q = deque([root])
+        while q:
+            size = len(q)
+            level = []
+            for _ in range(size):
+                node = q.popleft()
+                if node.left is not None:
+                    q.append(node.left)
+                if node.right is not None:
+                    q.append(node.right)
+                level.append(node.val)
+            ans.append(level)
+        return ans
